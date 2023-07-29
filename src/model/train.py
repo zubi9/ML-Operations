40efcb7f767154d9fd85674bc810eadb002a825d
Muhammad Zubair <zubimughal99@gmail.com>
2023-07-29 11:11:41 +0200

    Added split data fun

--- src/model/train.py
+++ src/model/train.py
@@ -36,10 +36,10 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
-def split_data(dataframe)
-    X = dataframe['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','age']
+def split_data(dataframe):
+    X = dataframe[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','age']]
     y = dataframe['Diabetic']
-    return X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return train_test_split(X, y, test_size=0.30, random_state=0)
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
