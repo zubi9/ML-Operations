ab135a50fa4f6e5cadd5ae063a48bf159b2a6b2f
Muhammad Zubair <zubimughal99@gmail.com>
2023-08-01 01:07:48 +0200

    Hyperparameter tuned

--- src/model/train.py
+++ src/model/train.py
@@ -54,7 +54,7 @@ def parse_args():
     parser.add_argument("--training_data", dest='training_data',
                         type=str)
     parser.add_argument("--reg_rate", dest='reg_rate',
-                        type=float, default=0.01)
+                        type=float, default=0.001)
 
     # parse args
     args = parser.parse_args()
